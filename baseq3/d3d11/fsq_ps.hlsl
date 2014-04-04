6ac92a7cf6c4d84c4909b64cf02f4ba56240f553
Pete Lewis <mygit@pjblewis.com>
2014-04-03 19:56:52 -0700

    Add the shaders

--- baseq3/d3d11/fsq_ps.hlsl
+++ baseq3/d3d11/fsq_ps.hlsl
@@ -1,11 +1,20 @@
 
+cbuffer CBuf : register(b0)
+{
+    float3 Color;
+};
+
+Texture2D Diffuse;
+
+SamplerState Sampler : register(s0);
+
 struct VS_PS_Data
 {
 	float4 Position : SV_POSITION;
-	float2 TexCoord : TEXCOORD0;
+	float2 TexCoord : TEXCOORD;
 };
 
 float4 Main(VS_PS_Data input) : SV_TARGET
 {
-	return float4(0, 1, 1, 1);
+	return float4(Color, 1) * Diffuse.Sample(Sampler, input.TexCoord);
 }
--- baseq3/d3d11/fsq_vs.hlsl
+++ baseq3/d3d11/fsq_vs.hlsl
@@ -1,21 +1,29 @@
 
+cbuffer ViewData : register(b0)
+{
+    float4x4 Projection;
+    float4x4 View;
+};
+
 struct VS_Data
 {
 	float2 Position : POSITION;
-	float2 TexCoord : TEXCOORD0;
+	float2 TexCoord : TEXCOORD;
 };
 
 struct VS_PS_Data
 {
 	float4 Position : SV_POSITION;
-	float2 TexCoord : TEXCOORD0;
+	float2 TexCoord : TEXCOORD;
 };
 
 VS_PS_Data Main(VS_Data input)
 {
 	VS_PS_Data output;
 
-	output.Position = float4(input.Position, 0, 1);
+	float4 viewPos = mul(View, float4(input.Position, 0, 1));
+
+	output.Position = mul(Projection, viewPos);
 	output.TexCoord = input.TexCoord;
 
 	return output;
