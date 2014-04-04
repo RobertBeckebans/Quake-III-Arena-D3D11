1cbd4b5db6a291e659034fe02a54f2a9a7533e8a
Pete Lewis <mygit@pjblewis.com>
2014-04-03 19:56:52 -0700

    Declaration int -> size_t for the VM (again)

--- code/qcommon/vm_local.h
+++ code/qcommon/vm_local.h
@@ -135,7 +135,7 @@ struct vm_s {
 
 	// for dynamic linked modules
 	void		*dllHandle;
-	int			(QDECL *entryPoint)( int callNum, ... );
+	int			(QDECL *entryPoint)( size_t callNum, ... );
 
 	// for interpreted modules
 	qboolean	currentlyInterpreting;
